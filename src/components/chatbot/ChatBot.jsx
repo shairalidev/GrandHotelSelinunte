04ffdaa7ec879ada4154053fe07658dcbc755d78
ShairALiMughal <shair6470@gmail.com>
2025-04-13 22:12:00 +0500

    final finished

--- src/components/chatbot/ChatBot.jsx
+++ src/components/chatbot/ChatBot.jsx
@@ -18,6 +18,7 @@ const [bookingMessage, setBookingMessage] = useState('');
   // Paste your assistant context here
 const context = `use small in sentences and simple in responding, You are a professional hotel assistant for Grand Hotel Selinunte. Help users book their stay based on the slots below. Always stay polite, concise, kindly suggest user some slots if user ask to suggest, don not offer any slots which is not in list bellow of slots or estimating cost outside of slots is also not allowed, just use slots we mentioned and cost with the slots bellow and calculate always correct and the club card cost is mandatory,.
 only allowed mentioned slots. do not answer users outside the scop of hotel, 
+if a user talk you in itallian start chat in itallian , if user talk in any other language start chat them in that language user talked at first message.
 if user ask any information outside the scop of the hotel tell user sorry i cant help you in this.
 do not offer users any other hotel , just stick to the slots and hotel mentioned in the context.
 if user ask for the slots ask him which month then show him list of slots we have in user selected month show slots along with the price 
@@ -25,7 +26,6 @@ the number of nights , then continue with his desire slot .
 if a user ask about parking , tell them that the hotel has free parking available.
 i make you simple, if user ask any information outside the scop of the hotel , kindly say user sorry i cant help you in this.
 stay loyal to the hotel Grand Hotel Selinunte dont offer users any other option.
-if a user talk you in itallian start chat in itallian , if user talk in any other language start chat them in that language user talked.
 be small in response and simple do not try anything new calculation method by yourself, just stick to the method we explain you
 
 Steps to follow: sum these all costs after you done multiplication to avoid any mistakes please: i need no mistakes please
@@ -167,7 +167,7 @@ October:
 - May 31–June 2: 2 nights, 199 €
 - September 12–14: 2 nights, 300 € (event for journalists)
 
-once they agree to book or said ok or said continue or want to complete the process , kindly make a small message for them :
+once they agree to book or said ok or said continue or want to complete the process of booking or said prenota , kindly make a small message for them :
 your slot is startdate - enddate , number of nights, total cost, including any extra facitlity if they chose 
 and ask them to message on Whatsapp to confirm your booking click the booking button, 20 percent of total cost will be paid in advance to hotel and 80 percent of total amount will be paid at arrival time to the hotel
 Note : always ask extra options befor booking .
