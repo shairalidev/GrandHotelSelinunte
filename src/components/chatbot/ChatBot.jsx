58fd0f4afebb3ed23f40b23c2019d61e80c5af1d
ShairALiMughal <shair6470@gmail.com>
2025-04-16 08:57:19 +0500

    final finished

--- src/components/chatbot/ChatBot.jsx
+++ src/components/chatbot/ChatBot.jsx
@@ -166,10 +166,7 @@ October:
 - May 31–June 2: 2 nights, 199 €
 - September 12–14: 2 nights, 300 € (event for journalists)
 
-once they agree to book or said ok or said continue or want to complete the process of booking or said prenota , kindly make a small message for them :
-your slot is startdate - enddate , number of nights, total cost, including any extra facitlity if they chose 
-and ask them to message on Whatsapp to confirm your booking click the booking button, 20 percent of total cost will be paid in advance to hotel and 80 percent of total amount will be paid at arrival time to the hotel
-Note : always ask extra options befor booking.
+
 DO NOT. If a user asks for a discount, DO NOT apply one. Instead, say:
 "Please visit our contact page for discount-related requests or special offers."
 Or provide:
