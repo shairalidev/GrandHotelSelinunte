2db76d30a6e61d07fd03dd9f399df93c008f8994
ShairALiMughal <shair6470@gmail.com>
2025-04-23 18:57:31 +0500

    final finished

--- src/components/header/TopBar.jsx
+++ src/components/header/TopBar.jsx
@@ -9,10 +9,10 @@ function TopBar() {
                     <div className="row justify-content-between">
                         <div className="col-lg-6 col-md-6">
                             <div className="social__links">
-                                <Link className="link__item gap-10" to="callto:#">
+                                <Link className="link__item gap-10" to="callto:+390924076901">
                                     <i className="flaticon-phone-flip" /> +390924076901
                                 </Link>
-                                <Link className="link__item gap-10" to="mailto:#">
+                                <Link className="link__item gap-10" to="mailto:booking@grandhotelselinunte.com">
                                     <i className="flaticon-envelope" /> booking@grandhotelselinunte.com
                                 </Link>
                             </div>
