95671df0dc60f190a6e432cd6f28ff525d3868c8
ShairALiMughal <shair6470@gmail.com>
2025-04-30 23:39:51 +0500

    final finished

--- src/pages/HomeDark.jsx
+++ src/pages/HomeDark.jsx
@@ -1,39 +1,42 @@
-import React, { useEffect } from 'react'
-import TopBar from '../components/header/TopBar'
-import HeaderDark from '../components/header/HeaderDark'
-import BannerOne from '../components/banner/BannerOne'
-import AdvanceForm from '../components/form/AdvanceForm'
-import FacilitiesOne from '../components/facilities/FalicitiesOne'
-import RoomOne from '../components/room/RoomOne'
-import TestimonialThree from '../components/testimonials/TestimonialThree'
-import VideoOne from '../components/videos/VideoOne'
-import GalleryOne from '../components/gallery/GalleryOne'
-import FooterOne from '../components/footer/FooterOne'
-import AboutFour from '../components/about/AboutFour'
+import React, { useEffect, lazy, Suspense } from 'react';
+
+const TopBar = lazy(() => import('../components/header/TopBar'));
+const HeaderDark = lazy(() => import('../components/header/HeaderDark'));
+const BannerOne = lazy(() => import('../components/banner/BannerOne'));
+const AdvanceForm = lazy(() => import('../components/form/AdvanceForm'));
+const AboutFour = lazy(() => import('../components/about/AboutFour'));
+const FacilitiesOne = lazy(() => import('../components/facilities/FalicitiesOne'));
+const RoomOne = lazy(() => import('../components/room/RoomOne'));
+const TestimonialThree = lazy(() => import('../components/testimonials/TestimonialThree'));
+const VideoOne = lazy(() => import('../components/videos/VideoOne'));
+const GalleryOne = lazy(() => import('../components/gallery/GalleryOne'));
+const FooterOne = lazy(() => import('../components/footer/FooterOne'));
+
 function HomeDark() {
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", "dark");
     return () => {
-      document.documentElement.removeAttribute("data-theme"); // Cleanup when leaving this page
+      document.documentElement.removeAttribute("data-theme");
     };
-
   }, []);
 
   return (
-    <div>
-      <TopBar />
-      <HeaderDark />
-      <BannerOne />
-      <AdvanceForm />
-      <AboutFour />
-      <FacilitiesOne />
-      <RoomOne className="section__padding"/>
-      <TestimonialThree className="is__home__one"/>
-      <VideoOne/>
-      <GalleryOne />
-      <FooterOne />
-    </div>
-  )
+    <Suspense fallback={<div>Caricamento...</div>}>
+      <div>
+        <TopBar />
+        <HeaderDark />
+        <BannerOne />
+        <AdvanceForm />
+        <AboutFour />
+        <FacilitiesOne />
+        <RoomOne className="section__padding" />
+        <TestimonialThree className="is__home__one" />
+        <VideoOne />
+        <GalleryOne />
+        <FooterOne />
+      </div>
+    </Suspense>
+  );
 }
 
-export default HomeDark
+export default HomeDark;
