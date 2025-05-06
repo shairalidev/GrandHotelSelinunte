1f539ca876ae89797edc1fe2b53bf672732340c1
ShairALiMughal <shair6470@gmail.com>
2025-05-07 00:07:50 +0500

    final finished

--- src/components/about/AboutFive.jsx
+++ src/components/about/AboutFive.jsx
@@ -21,7 +21,7 @@ function AboutFive() {
                             >
                                 Benvenuti in Italia, dove il lusso incontra il comfort nel cuore di Marinella. 
                                 Da sempre ci dedichiamo a offrire un soggiorno eccezionale ai nostri ospiti, 
-                                unendo servizi moderni a un’eleganza senza tempo. Le nostre camere e suite, 
+                                unendo servizi moderni a un’eleganza senza tempo. Le nostre camere, 
                                 splendidamente arredate, offrono viste mozzafiato e sistemazioni raffinate, 
                                 garantendo un rifugio rilassante sia per viaggi di lavoro che di piacere.
                             </p>
